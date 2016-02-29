19ced984c8f866d5545eea2c383b21df672f9257
honger <honger05@126.com>
2016-03-01 01:16:56 +0800

    f

--- gallery/common/common.js
+++ gallery/common/common.js
@@ -1,9 +1,9 @@
 
 // require('font-awesome/scss/font-awesome.scss')
-require('../styles/common.scss')
 require('../ionic/scss/ionic.scss')
+require('../styles/common.scss')
 
-require('../ionic/release/js/ionic.bundle')
+// require('../ionic/release/js/ionic.bundle')
 
 module.exports = {
 
